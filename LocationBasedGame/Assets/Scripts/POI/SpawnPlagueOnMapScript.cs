971deb2521069202737f211867b36baa245f97ff
0t1 <marvin.otec@hs-furtwangen.de>
2019-07-25 11:33:27 +0200

    fixes

---
+++ LocationBasedGame/Assets/Scripts/POI/SpawnPlagueOnMapScript.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using Mapbox.Utils;
+using Mapbox.Unity.Map;
+using Mapbox.Unity.MeshGeneration.Factories;
+using Mapbox.Unity.Utilities;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class SpawnPlagueOnMapScript : MonoBehaviour
+{
+    [SerializeField]
+    AbstractMap map;
+
+    [SerializeField]
+    [Geocode]
+    string[] locationStrings;
+    Vector2d[] locations;
+
+    [SerializeField]
+    float spawnScale = 100f;
+
+    [SerializeField]
+    GameObject markerPrefab;
+
+    List<GameObject> spawnedObjects;
+
+    void Start()
+    {
+        Debug.Log(PhotonNetwork.IsConnected);
+        Debug.Log(PhotonNetwork.IsMasterClient);
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            spawnPlague();
+        }
+    }
+
+    private void Update()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            updatePlaguePositon();
+        }
+    }
+
+    private void spawnPlague()
+    {
+        locations = new Vector2d[locationStrings.Length];
+        spawnedObjects = new List<GameObject>();
+        for (int i = 0; i < locationStrings.Length; i++)
+        {
+            var locationString = locationStrings[i];
+            locations[i] = Conversions.StringToLatLon(locationString);
+            var instance = Instantiate(markerPrefab);
+            instance.transform.localPosition = map.GeoToWorldPosition(locations[i], true);
+            instance.transform.localScale = new Vector3(spawnScale, spawnScale, spawnScale);
+            spawnedObjects.Add(instance);
+        }
+    }
+
+    private void updatePlaguePositon()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            int count = spawnedObjects.Count;
+            for (int i = 0; i < count; i++)
+            {
+                var spawnedObject = spawnedObjects[i];
+                var location = locations[i];
+                spawnedObject.transform.localPosition = map.GeoToWorldPosition(location, true);
+                spawnedObject.transform.localScale = new Vector3(spawnScale, spawnScale, spawnScale);
+            }
+        }
+    }
+}
