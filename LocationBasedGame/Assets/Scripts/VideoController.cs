87fb7536b521d180a815d507543d66dcc73a8548
nincara <corinna.braun94@web.de>
2019-07-26 00:57:28 +0200

    added intro video

---
+++ LocationBasedGame/Assets/Scripts/VideoController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour
+{
+    private VideoPlayer videoPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer = GameObject.Find("VideoPlayer").GetComponent<VideoPlayer>();
+        GameObject.Find("VideoPlayer").SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        videoPlayer.loopPointReached += EndReached;
+    }
+
+    public void stopVideo()
+    {
+        videoPlayer.Stop();
+        GameObject.Find("VideoPlayer").SetActive(false);
+    }
+
+    void EndReached(UnityEngine.Video.VideoPlayer vp)
+    {
+        GameObject.Find("VideoPlayer").SetActive(false);
+    }
+}
