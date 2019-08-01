a53cf9f5df4ebd532b9135d7fb33c25da5543f41
Marvin Otec <marvin.otec@hs-furtwangen.de>
2019-08-01 14:26:33 +0200

    Update SqliteHelper.cs
    Added missing reference

--- LocationBasedGame/Assets/Scripts/Database/SqliteHelper.cs
+++ LocationBasedGame/Assets/Scripts/Database/SqliteHelper.cs
@@ -5,7 +5,7 @@ using System.Text;
 using Mono.Data.Sqlite;
 using UnityEngine;
 using System.Data;
-// NULLt1
+//https://medium.com/@rizasif92/sqlite-and-unity-how-to-do-it-right-31991712190
 namespace DatabaseN
 {
     public class SqliteHelper
